0697816b1e2e183a81835b24911959a7af00740e
don <w0156394@nscc.ca>
2014-02-18 01:10:43 -0400

    Committed

---
+++ confirm_Assignment1.php
@@ -0,0 +1,21 @@
+<?php
+//show all informaion and get confirmation
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Student Course Tracker</title>
+		<link rel='stylesheet' type='text/css' href='StyleSheet.css' />
+	</head>
+	<body>
+		<h1>Student Course Tracker <?  ?></h1>
+		<form method="POST" action="work_Assignment1.php">	  
+			<fieldset>
+				<legend>Student Info</legend>				
+				
+			</fieldset>
+			<p><input type="SUBMIT" name="wrkSubmit" value="Confirm"></p>
+		</form>
+	</body>
+</html>
+
---
+++ work_Assignment1.php
@@ -0,0 +1,21 @@
+<?php
+//email and update files
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Student Course Tracker</title>
+		<link rel='stylesheet' type='text/css' href='StyleSheet.css' />
+	</head>
+	<body>
+		<h1>Student Course Tracker <?  ?></h1>
+		<form method="POST" action="show_Assignment1.html"><!--  -->	  
+			<fieldset>
+				<legend>Student Info</legend>				
+				
+			</fieldset>
+			<p><input type="SUBMIT" name="rtrnSubmit" value="Return To Main Page"></p>
+		</form>
+	</body>
+</html>
