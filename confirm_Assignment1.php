6d6ce32a4e0bac1b8cd2c7038040d8aa5b1b10cb
don <w0156394@nscc.ca>
2014-02-18 20:39:38 -0400

    Committed

--- confirm_Assignment1.php
+++ confirm_Assignment1.php
@@ -1,13 +1,63 @@
 <?php
 //show all informaion and get confirmation
-	echo $_FILES['img1']['name'];
-	echo $_FILES['img1']['size'];
-	echo $_FILES['img1']['type'];
-	if ($_FILES['img1'] != ""){
-		@copy($_FILES['img1']['tmp_name'], "../../files/img/".$_FILES['img1']['name'] ) or die ("could not copy the file");
-		
-	} else {
-		die ("no input file specified");
+
+	$ftp_server = "php.nscctruro.ca";
+	$ftp_username   = "w0156394";
+	$ftp_password   =  "w0156394";
+	$conn_id = ftp_connect($ftp_server) or die("could not connect to $ftp_server");
+	//connected
+	if(@ftp_login($conn_id, $ftp_username, $ftp_password)){
+	
+		if($_POST['txtFirstName']!="" && $_POST['txtLastName']!="" && $_POST['txtEmail']!="" && 
+			$_POST['txtEmail']!="" &&  $_POST['selFirstCourses']!="null" &&  $_POST['selSecondCourses']!="null"
+			&&  $_POST['selThirdCourses']!="null" &&  $_POST['selForthCourses']!="null") {
+			
+			if(eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $_POST['txtEmail'])) {   
+				//use ftp_put to upload the image
+				ftp_put($conn_id, "../../files/img/".$_FILES['img1']['name'], $_FILES['img1']['tmp_name'],FTP_ASCII);
+												
+				
+				//fill variables
+				$firstName = $_POST['txtFirstName'];
+				$lastName = $_POST['txtLastName'];
+				$email = $_POST['txtEmail'];
+				$dOB = $_POST['txtDOB'];
+				$firstCourses = $_POST['selFirstCourses'];
+				$secondCourses = $_POST['selSecondCourses'];
+				$thirdCourses = $_POST['selThirdCourses'];
+				$forthCourses = $_POST['selForthCourses'];
+				
+				//populate email message
+				$conMsg = "First Name: $firstName <br/>				
+						Last Name: $lastName <br/>	
+						Email: $email <br/>	
+						Date Of Birth: $dOB <br/>	
+						First Courses: $firstCourses <br/>	
+						Second Courses: $secondCourses <br/>	
+						Third Courses: $thirdCourses <br/>	
+						Forth Courses: $forthCourses  <br/>	
+						Image: 
+						<img src='../../files/img/".$_FILES['img1']['name']."'
+						alt='Image Of Student' height='42' width='42'>	\n";
+				
+				//format first name with last initial
+				$lastNameExp = explode(" ", $lastName);
+				$firstLetterOfLast = "";	
+				//get first letter of last name
+				foreach ($lastNameExp as $l) {
+					$firstLetterOfLast = $l[0];
+					if ($firstLetterOfLast != ""){break;}	  
+				}		
+				$nameToAdd = $firstName . " " . $firstLetterOfLast . "\n";
+				//echo "name to add " . $nameToAdd ;
+				//echo "$firstName, $lastName, $email, $dOB, $firstCourses,$secondCourses, $thirdCourses, $forthCourses";			
+			} else {   
+				echo "Invalid email address."; 
+				$conMsg = "";
+			}  
+		} else {
+			echo "You must fill in all the fields";
+		}
 	}
 ?>
 <!DOCTYPE html>
@@ -18,13 +68,19 @@
 	</head>
 	<body>
 		<h1>Student Course Tracker <?  ?></h1>
-		<form method="POST" action="work_Assignment1.php">	  
+		<form method="POST" action="work_Assignment1.php"  ENCTYPE="multipart/form-data">	  
 			<fieldset>
-				<legend>Student Info</legend>				
-				
+				<legend>Student Info</legend>
+				<? echo $conMsg ?>				
+				<input name="lblHdmMsg" type="hidden" value="<? echo $conMsg; ?>" />
+				<input name="lblHdmEmail" type="hidden" value="<? echo $email; ?>" />
+				<input name="lblHdmFirstL" type="hidden" value="<? echo $nameToAdd; ?>" />
+				<input name="lblHdmC1" type="hidden" value="<? echo $firstCourses; ?>" />
+				<input name="lblHdmC2" type="hidden" value="<? echo $secondCourses; ?>" />
+				<input name="lblHdmC3" type="hidden" value="<? echo $thirdCourses; ?>" />
+				<input name="lblHdmC4" type="hidden" value="<? echo $forthCourses; ?>" />
 			</fieldset>
 			<p><input type="SUBMIT" name="wrkSubmit" value="Confirm"></p>
 		</form>
 	</body>
 </html>
-
--- work_Assignment1.php
+++ work_Assignment1.php
@@ -1,5 +1,35 @@
-<?php
-//email and update files with names
+<?php		
+	//Enter the name to the file(s)
+	$nameToAdd = $_POST['lblHdmFirstL'];
+	$aryCourses = array($_POST['lblHdmC1'], $_POST['lblHdmC2'], $_POST['lblHdmC3'], $_POST['lblHdmC4']); 
+
+	foreach ($aryCourses as $b) { 
+		$filename = "../../files/courses/$b";
+		$myFile = @fopen($filename, "a+") or die ("failed to open student name file");
+		@fwrite($myFile, $nameToAdd) or die ("failed to write student name to file");
+		@fclose($myFile);
+		echo " $nameToAdd added to $b";
+	} 
+	
+	//it's ok to send, so build the email and send it
+	$msg = "";
+	$msg = $_POST['lblHdmMsg'];
+	//$msg = "E-MAIL SENT FROM it.nscctruro.ca\n";
+	//$msg .= "Sender's Name:    $_POST[sender_name]\n";
+	//$msg .= "Sender's E-Mail:  $_POST[sender_email]\n";
+	//$msg .= "Message:          $_POST[message]\n\n";
+	//$msg .= "Time:          ";
+	//$msg .= date("F j, Y, g:i a");       
+
+	$to = "dlnmcnutt@gmail.com";
+	$subject = "All-in-One Web Site Feedback";
+	$mailheaders = "From:it.nscctruro.ca 
+	<webmaster@nscctruro.ca>\n";
+	//$mailheaders .= "Reply-To: $_POST['lblHdmEmail']\n";
+	//$mailheaders .= "CC: $_POST['lblHdmEmail']";
+	
+	//send the mail
+	mail($to, $subject, $msg, $mailheaders);
 
 ?>
 <!DOCTYPE html>
@@ -12,8 +42,9 @@
 		<h1>Student Course Tracker <?  ?></h1>
 		<form method="POST" action="show_Assignment1.html"><!--  -->	  
 			<fieldset>
-				<legend>Student Info</legend>				
-				
+				<legend>Student Info</legend>	
+				<? echo "<p>Mail has been sent:</p>"; ?>
+				<? echo $msg; ?>
 			</fieldset>
 			<p><input type="SUBMIT" name="rtrnSubmit" value="Return To Main Page"></p>
 		</form>
